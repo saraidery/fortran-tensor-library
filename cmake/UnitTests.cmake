f5ad71c91b360dc3f2e80b2ba2f43d35a06fe78b
Eirik F. Kjønstad <eirik.kjonstad@ntnu.no>
2021-11-16 15:57:53 +0100

    Deleted program, now only library, and added first unit test

--- cmake/UnitTests.cmake
+++ cmake/UnitTests.cmake
@@ -1,18 +1,18 @@
 
 macro(add_ftensor_unittest _name _labels)
 
-   set (filename unit_${_name})
+   set (filename ${_name})
+
+   file (COPY ${CMAKE_SOURCE_DIR}/tests/${_name}.pf 
+         DESTINATION ${CMAKE_BINARY_DIR})
 
    add_pfunit_ctest (${filename}
       TEST_SOURCES ${CMAKE_BINARY_DIR}/${filename}.pf
-      LINK_LIBRARIES ftensor_library
+      LINK_LIBRARIES ftensor_library ${EXTRA_LINKER_FLAGS}
    )
-
-   # add "unit-test" as a label for all unit tests
-   string(CONCAT labels "${_labels}" "; unit-test")
 
    set_tests_properties(${filename} PROPERTIES LABELS "${labels}")
 
 endmacro()
 
-#add_ftensor_unittest(warning_suppressor "dummy; warning-suppression")
+add_ftensor_unittest(create_tensor "tensor; constructor")
--- src/ftensor_program.F90
+++
@@ -1,9 +0,0 @@
-program ftensor
-!
-   use tensor_class, only: tensor
-!
-   type(tensor) :: my_tensor
-!
-   my_tensor = tensor([5, 100, 2, 6])
-!
-end program ftensor

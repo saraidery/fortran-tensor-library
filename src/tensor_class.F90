1fc84e7360d93a8fb9cbc70ea0411281678363a0
Eirik F. Kjønstad <eirik.kjonstad@ntnu.no>
2021-11-16 15:01:17 +0100

    Made n_elements in tensor and initialize/destruct

--- src/tensor_class.F90
+++ src/tensor_class.F90
@@ -1,14 +1,27 @@
 module tensor_class
-!
+!!
+!!    Tensor class
+!!
+!!    Class for arbitrary-rank tensors. Stores the tensor and handles
+!!    its indices and total dimensionality.
+!!
    use kinds
 !
    type :: tensor
 !
-      integer :: rank
+      integer, private :: rank
+!
+      integer, private :: n_elements 
 !
       integer, dimension(:), allocatable, private :: dimensions
 !
       real(dp), dimension(:), allocatable, private :: array
+!
+   contains
+!
+      procedure, public :: initialize   
+!
+      final :: destructor 
 !
    end type tensor
 !
@@ -20,13 +33,18 @@ module tensor_class
 !
 contains
 !
-   function new_tensor(dimensions) result(this)
 !
+   function new_tensor(dimensions) result(this)
+!!
+!!    New tensor
+!!
       implicit none
 !
       type(tensor) :: this
 !
-      integer, dimension(:) :: dimensions
+      integer, dimension(:), intent(in) :: dimensions
+!
+      integer :: k
 !
       this%rank = size(dimensions)
 !
@@ -34,8 +52,48 @@ contains
 !
       this%dimensions = dimensions
 !
-      print*, "I am a tensor with rank", this%rank, "and dimensions", this%dimensions
+      this%n_elements = 1 
+!
+      do k = 1, this%rank 
+!
+         this%n_elements = this%n_elements * this%dimensions(k)
+!
+      enddo
+!
+      print*, "Created tensor object with:"
+!
+      print*, ""
+      print*, "Rank: ", this%rank 
+      print*, "Dimensions: ", this%dimensions
+      print*, "Number of elements: ", this%n_elements
 !
    end function new_tensor
+!
+!
+   subroutine initialize(this)
+!!
+!!    Initialize
+!!
+      implicit none 
+!
+      class(tensor), intent(inout) :: this 
+!
+      allocate(this%array(this%n_elements))
+!
+   end subroutine initialize 
+!
+!
+   subroutine destructor(this)
+!!
+!!    Destructor
+!!
+      implicit none 
+!
+      type(tensor), intent(inout) :: this 
+!
+      if (allocated(this%array)) deallocate(this%array)
+!
+   end subroutine destructor
+!
 !
 end module tensor_class

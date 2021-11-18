17bfff5d0dcd260f046949c82e3e3304defc3b9a
Alex <alexander.c.paul@ntnu.no>
2021-11-18 11:10:05 +0100

    find_substring and tests

--- src/string_class.F90
+++ src/string_class.F90
@@ -14,12 +14,14 @@ module string_class
 !
       procedure, public :: get_length
       procedure, public :: print_
+      procedure, public :: find_substring
 !
    end type string
 !
    interface string
 !
       procedure :: new_string
+      procedure :: copy_string
 !
    end interface string
 !
@@ -41,6 +43,21 @@ contains
       this%length = len(characters)
 !
    end function new_string
+!
+!
+   function copy_string(that) result(this)
+!!
+!!    Copy string
+!!
+      implicit none
+!
+      type(string), intent(in) :: that
+      type(string) :: this
+!
+      this%string = that%string
+      this%length = that%length
+!
+   end function copy_string
 !
 !
    subroutine print_(this)
@@ -69,6 +86,53 @@ contains
       length = this%length
 !
    end function get_length
+!
+!
+   subroutine find_substring(this, substring, start_index, found)
+!!
+!!    Find substring
+!!
+!!    Adapted from string_utilities.F90 by Sarai D. Folkestad
+!!    (https://gitlab.com/eT-program/eT/-/blob/0814712a2c4cd4e931361aaaa267ee7cea877f40/src/tools/string_utilities.F90)
+!!    licensed under GPLv3.
+!!
+!!    Returns index of the first matching character in the string
+!!
+      implicit none
+!
+      class(string), intent(in) :: this
+      character(len=*), intent(in)  :: substring
+!
+      integer, intent(out) :: start_index
+      logical, intent(out) :: found
+!
+      integer :: substring_length, cursor
+!
+      substring_length = len(trim(substring))
+!
+      start_index = 1
+      cursor = start_index + substring_length - 1
+!
+      found = .false.
+!
+      do while (cursor .le. len(this%string))
+!
+         if (this%string(start_index : cursor) == trim(substring)) then
+!
+            found = .true.
+            exit
+!
+         else
+!
+            start_index = start_index + 1
+            cursor = cursor + 1
+!
+         endif
+      enddo
+!
+      if (.not. found) start_index = 0
+!
+   end subroutine find_substring
 !
 !
 end module string_class
